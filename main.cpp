25ce2e680a87286c37270b54c0d24ca35d8d83da
DanielUtec123 <34191864+DanielUtec123@users.noreply.github.com>
2019-04-02 15:29:18 -0500

    accepted solution

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
-
+#include <string>
+#include <map>
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
+    int n;
+    map<string,int> paises_m;
+
+    cin >> n;
+
+    string pais,nombre,paises[n];
+
+    for (int i=0;i<n;i++){
+        cin>> pais;
+        getline(cin,nombre);
+        paises_m[pais]=0;
+        paises[i]=pais;
+    }
+
+    for (int i=0;i<n;i++)
+    {
+        paises_m[paises[i]]++;
+    }
+
+    for(std::map<string,int>::iterator it = paises_m.begin();it != paises_m.end();it++)
+    {
+
+        cout << it->first << " " << it->second<< endl;
+    }
     return 0;
 }
